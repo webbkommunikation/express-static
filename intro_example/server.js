182ef8a383d6a4d4038d4bc9db3e181acd6470f0
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2024-11-25 09:54:29 +0100

    POST ON DOM

--- intro_example/server.js
+++ intro_example/server.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/greeting", (req, res) => {
-    res.json({ "message": "Hello from the API!" })
-})
+  res.json({ message: "Hello from the API!" });
+});
 
 app.post("/submit", (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
-})
+  console.log(req.body);
+  res.send("Posted message");
+});
 
 app.listen(3000, () => {
-    console.log("Server is running.")
-})
+  console.log("Server is running.");
+});
